80f6285945b1fdf08e4e7ee73c171015694e5960
Edison Alvarez <edisonav1991@gmail.com>
2025-03-03 18:45:14 -0500

    Primer avance

---
+++ data_structures_activity_2/GeometricFigure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresActivity_2;
+public abstract class GeometricFigure
+{
+    public string? Name { get; set; }
+
+    public abstract double GetArea();
+    public abstract double GetPerimeter();
+
+    public override string ToString()
+    {
+        return $"Figura: {Name}, Área: {GetArea()}, Perímetro: {GetPerimeter()}";
+    }
+}
---
+++ data_structures_activity_2/Square.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresActivity_2;
+
+public class Square : GeometricFigure
+{
+    private decimal _a;
+
+    public Square(decimal _a)
+    {
+        _A = _a;
+
+    }
+
+    public decimal _A { get => _a; set => _a = value; }
+
+    public override double GetArea()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override double GetPerimeter()
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal square 
+}
